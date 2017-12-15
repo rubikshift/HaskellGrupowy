372dcce6ef8f227bf99bf0614445e5b7fadef36f
Michał Krakowiak <michal.krak97@gmail.com>
2017-12-15 17:10:31 +0100

    poprawa bledu

--- main.hs
+++ main.hs
@@ -3,11 +3,11 @@ data Zdanie = Z Char | N Zdanie | C Zdanie Zdanie | A Zdanie Zdanie | K Zdanie Z
 drukuj :: Zdanie -> [Char]
 -- definicje drukuj dla roznych werjsi zdania, haskell dopasuje sie do ktoregos z nich
 -- analogicznie trzeba zrobic dla nastepnych
-drukuj (Z Char) = ""
-drukuj (N Zdanie) = ""
-drukuj (C Zdanie Zdanie) = ""
-drukuj (A Zdanie Zdanie) = ""
-drukuj (K Zdanie Zdanie) = ""
+drukuj (Z znak) = ""
+drukuj (N zdanie) = ""
+drukuj (C zdanie1 zdanie2) = ""
+drukuj (A zdanie1 zdanie2) = ""
+drukuj (K zdanie1 zdanie2) = ""
 
 usun_duplikaty :: [Char] -> [Char]
 usun_duplikaty s = ""
