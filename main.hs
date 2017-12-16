5d79d5407da9e3955ab2c76749977e1038911a57
Michał Krakowiak <michal.krak97@gmail.com>
2017-12-16 16:07:37 +0100

    czy jest tautologia

--- main.hs
+++ main.hs
@@ -1,7 +1,9 @@
-import Data.Map (Map)
+import Data.List
+import qualified Data.Map as Map
 
 data Zdanie = Z Char | N Zdanie | C Zdanie Zdanie | A Zdanie Zdanie | K Zdanie Zdanie
 
+--zadanie1
 drukuj :: Zdanie -> [Char]
 -- drukuj dopasuje sie do formatu zdania (dekonstrukcja typu), wystarczy obsluzyc podstawowe przypadki
 drukuj (Z znak) = [znak]
@@ -10,6 +12,7 @@ drukuj (C zdanie1 zdanie2) = "(" ++ (drukuj zdanie1) ++ " => " ++ (drukuj zdanie
 drukuj (A zdanie1 zdanie2) = "(" ++ (drukuj zdanie1) ++ " | " ++ (drukuj zdanie2) ++ ")"
 drukuj (K zdanie1 zdanie2) = "(" ++ (drukuj zdanie1) ++ " & " ++ (drukuj zdanie2) ++ ")"
 
+--zadanie2
 zmienne :: Zdanie ->[Char] 
 zmienne (Z znak)  =[znak]
 zmienne  (N zdanie) = zmienne zdanie 
@@ -20,14 +23,14 @@ zmienne  (K zdanie1 zdanie2) = (zmienne zdanie1 )++ (zmienne zdanie2 )
 usun_duplikaty :: [Char] ->[Char]
 usun_duplikaty [] = []
 usun_duplikaty (a:b) 
-	|  czypowtorka b a = usun_duplikaty b 
-	| otherwise =  a : usun_duplikaty b
+    |  czypowtorka b a = usun_duplikaty b 
+    | otherwise =  a : usun_duplikaty b
 
 czypowtorka :: [Char] -> Char -> Bool
 czypowtorka [] x= False
 czypowtorka (a:b)  x
-	|  a == x = True 
-	|  otherwise = czypowtorka b x
+    |  a == x = True 
+    |  otherwise = czypowtorka b x
 
 
 dodaj_granice :: [Char] -> [Char]
@@ -38,19 +41,46 @@ dodaj_granice x = '[' : x ++ ']': []
 wypisz_zmienne :: [Char] -> [Char]
 wypisz_zmienne[]=[]
 wypisz_zmienne (a:b)
-	| a == '[' = a: wypisz_zmienne b
-	| a == ']' = a: wypisz_zmienne b
-	| b == [']'] = a: wypisz_zmienne b
-	| otherwise = a :[','] ++ wypisz_zmienne b
+    | a == '[' = a: wypisz_zmienne b
+    | a == ']' = a: wypisz_zmienne b
+    | b == [']'] = a: wypisz_zmienne b
+    | otherwise = a :[','] ++ wypisz_zmienne b
 
-sprawdz :: Zdanie -> Map Char Bool -> Bool
+-- zadanie3
+sprawdz :: Zdanie -> Map.Map Char Bool -> Bool
 sprawdz z m = False
 
---Funkcja do zad4
+-- zadanie4
+--konwersja liczby na 'reprezentacje binarna' (tablica wartosci logicznych) => wygenerowanie n-tej kombinacji wartosci
+zapisBinarny :: Int -> Int -> [Bool]
+zapisBinarny 0 x = []
+zapisBinarny d 0 = [False | i <- [1..d]]
+zapisBinarny d x = do
+    if k == 0 then False : (zapisBinarny n x) 
+    else True : (zapisBinarny n (x - k*m))
+    where 
+        n = d - 1
+        m = 2^n
+        k = x `div` m
+
+--wygenerowanie wszystkich mozliwych wartosci
+generuj_wartosci :: [Char] -> [[(Char, Bool)]]
+generuj_wartosci [] = []
+generuj_wartosci z = [ zip z w | w <- (map (zapisBinarny n) [0 .. 2^n - 1])]
+    where
+        n = length(z)
+
+--konwersja wygenerowanych wartosci na liste hashmap
+stworz_mapy :: [[(Char, Bool)]] -> [Map.Map Char Bool]
+stworz_mapy m = [Map.fromList x | x <- m]
+
+--sprawdzenie zdania dla kazdej wygenerowanej mapy wartosci, nastepnie iloczyn logiczny wszystkich wartosci
+jest_tautologia :: Zdanie -> Bool
+jest_tautologia z = (foldr (&&) True [ sprawdz z x | x <- (stworz_mapy . generuj_wartosci . usun_duplikaty . zmienne) z ]) == True
 
 testowe_zdanie = (C (N (Z 'p')) (A (K (Z 'p') (Z 'q')) (Z 'r')))
 
 main :: IO ()
 main = do
-putStrLn (drukuj testowe_zdanie)
-putStrLn (wypisz_zmienne(dodaj_granice(usun_duplikaty(zmienne testowe_zdanie))))
+    putStrLn (drukuj testowe_zdanie)
+    putStrLn (wypisz_zmienne(dodaj_granice(usun_duplikaty(zmienne testowe_zdanie))))
