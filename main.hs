611d884fedf62fd6e848df52f9f5e0bef0a10db7
polom11 <s165324@student.pg.gda.pl>
2017-12-16 17:28:47 +0100

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,9 +1,11 @@
+import Data.Maybe
 import Data.List
-import qualified Data.Map as Map
+import Data.Map 
+
+
 
 data Zdanie = Z Char | N Zdanie | C Zdanie Zdanie | A Zdanie Zdanie | K Zdanie Zdanie
 
---zadanie1
 drukuj :: Zdanie -> [Char]
 -- drukuj dopasuje sie do formatu zdania (dekonstrukcja typu), wystarczy obsluzyc podstawowe przypadki
 drukuj (Z znak) = [znak]
@@ -12,7 +14,6 @@ drukuj (C zdanie1 zdanie2) = "(" ++ (drukuj zdanie1) ++ " => " ++ (drukuj zdanie
 drukuj (A zdanie1 zdanie2) = "(" ++ (drukuj zdanie1) ++ " | " ++ (drukuj zdanie2) ++ ")"
 drukuj (K zdanie1 zdanie2) = "(" ++ (drukuj zdanie1) ++ " & " ++ (drukuj zdanie2) ++ ")"
 
---zadanie2
 zmienne :: Zdanie ->[Char] 
 zmienne (Z znak)  =[znak]
 zmienne  (N zdanie) = zmienne zdanie 
@@ -23,14 +24,14 @@ zmienne  (K zdanie1 zdanie2) = (zmienne zdanie1 )++ (zmienne zdanie2 )
 usun_duplikaty :: [Char] ->[Char]
 usun_duplikaty [] = []
 usun_duplikaty (a:b) 
-    |  czypowtorka b a = usun_duplikaty b 
-    | otherwise =  a : usun_duplikaty b
+	|  czypowtorka b a = usun_duplikaty b 
+	| otherwise =  a : usun_duplikaty b
 
 czypowtorka :: [Char] -> Char -> Bool
 czypowtorka [] x= False
 czypowtorka (a:b)  x
-    |  a == x = True 
-    |  otherwise = czypowtorka b x
+	|  a == x = True 
+	|  otherwise = czypowtorka b x
 
 
 dodaj_granice :: [Char] -> [Char]
@@ -49,41 +50,30 @@ formatuj  (a:b)
 wypisz_zmienne :: Zdanie -> IO()
 wypisz_zmienne z = putStrLn (formatuj(dodaj_granice(usun_duplikaty(zmienne z))))
 
--- zadanie3
-sprawdz :: Zdanie -> Map.Map Char Bool -> Bool
-sprawdz z m = False
 
--- zadanie4
---konwersja liczby na 'reprezentacje binarna' (tablica wartosci logicznych) => wygenerowanie n-tej kombinacji wartosci
-zapisBinarny :: Int -> Int -> [Bool]
-zapisBinarny 0 x = []
-zapisBinarny d 0 = [False | i <- [1..d]]
-zapisBinarny d x = do
-    if k == 0 then False : (zapisBinarny n x) 
-    else True : (zapisBinarny n (x - k*m))
-    where 
-        n = d - 1
-        m = 2^n
-        k = x `div` m
 
---wygenerowanie wszystkich mozliwych wartosci
-generuj_wartosci :: [Char] -> [[(Char, Bool)]]
-generuj_wartosci [] = []
-generuj_wartosci z = [ zip z w | w <- (map (zapisBinarny n) [0 .. 2^n - 1])]
-    where
-        n = length(z)
+sprawdz :: Zdanie -> Map Char Bool -> Bool
+sprawdz (Z znak) m = findWithDefault False znak m
+sprawdz  (N zdanie) m=not(sprawdz zdanie m)
+sprawdz  (C zdanie1 zdanie2) m = not(sprawdz zdanie1 m) || (sprawdz zdanie2 m)
+sprawdz  (A zdanie1 zdanie2) m =(sprawdz zdanie1 m) || (sprawdz zdanie2 m)
+sprawdz  (K zdanie1 zdanie2) m= (sprawdz zdanie1 m) && (sprawdz zdanie2 m)
 
---konwersja wygenerowanych wartosci na liste hashmap
-stworz_mapy :: [[(Char, Bool)]] -> [Map.Map Char Bool]
-stworz_mapy m = [Map.fromList x | x <- m]
 
---sprawdzenie zdania dla kazdej wygenerowanej mapy wartosci, nastepnie iloczyn logiczny wszystkich wartosci
-jest_tautologia :: Zdanie -> Bool
-jest_tautologia z = (foldr (&&) True [ sprawdz z x | x <- (stworz_mapy . generuj_wartosci . usun_duplikaty . zmienne) z ]) == True
+--Funkcja do zad4
 
 testowe_zdanie = (C (N (Z 'p')) (A (K (Z 'p') (Z 'q')) (Z 'r')))
+mapa_wartosci =  fromList [('p', False), ('q', True), ('r', False)]
 
 main :: IO ()
 main = do
-    putStrLn (drukuj testowe_zdanie)
-    wypisz_zmienne testowe_zdanie
+putStrLn (drukuj testowe_zdanie)
+wypisz_zmienne testowe_zdanie
+print(sprawdz testowe_zdanie mapa_wartosci)
+
+
+
+
+
+
+
